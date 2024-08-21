8e5942a034562e70f2f4b1d207a074dac3ce1f9d
SimBienvenueHoulBoumi <brhulla@gmail.com>
2024-08-21 14:36:33 +0200

    🎨 Icon
    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Nutrilab admin",
   description: "nutrilab admin interface",
+  icons: {
+    icon: ["/favicon.ico"],
+  },
 };
 
 export default function RootLayout({
